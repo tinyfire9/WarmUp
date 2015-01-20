c5f959b7087ccb93620178a91fc18761ae548eea
Yohannes <personalprojectyohannes@gmail.com>
2015-01-19 23:43:45 -0600

    [FEATURE] add client schema

---
+++ models/model.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+mongoose.connect('mongodb://db_username:db_password@ds031711.mongolab.com:31711/heroku_app33339190');
+
+var clientSchema = new schema({
+	 phoneNumber : Number,
+	 zipcode : Number,
+	 notificationTemprature : Number
+}, {collection : 'weatherNotification'});
+
+var clientModel = mongoose.model('weatherNotification', clientSchema);
+
+exports.clientModel = clientModel;
