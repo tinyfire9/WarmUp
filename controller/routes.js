e70553110c59ed57add8e46e902ea6f362173b21
Yohannes <personalprojectyohannes@gmail.com>
2015-01-21 12:32:36 -0600

    [FEATURE] send SMS message upon subscription

--- controller/routes.js
+++ controller/routes.js
@@ -1,20 +1,47 @@
-var Recipient = require('../library/recipient.js');
+var WarmUp = require('../library/warmUp.js');
 
-var Recipient = new Recipient();
+var WarmUp = new WarmUp();
 
 exports.indexResponseHandler = function(req, res){
 	res.render('index');
 }
 
 exports.addRecipientResponseHandler = function(req, res){
-	Recipient.addRecipient(req.body.phoneNumber, req.body.zipcode, req.body.notificationTemprature, function(error, status){
+	var messageBody;
+	WarmUp.currentTemprature(req.body.zipcode, function(error, weatherData){
 		if(error)
 		{
 			throw Error(error);
 		}
-		if(status == "success")
+		if(!weatherData)
 		{
-			res.render('index', { submitted : true});
+			return (res.render('index', { submitted : false,
+								  error: "Oops! The zipcode, " + req.body.zipcode + ", does not exists!"
+								}));
 		}
+		messageBody = "Hi, Thanks for subscribing! The current temprature in " + weatherData.city + " is " + weatherData.temprature + ". We will notify you when the temprature is below " + req.body.notificationTemprature + "every 24 hours. -warmUp";
+		WarmUp.sendsms(req.body.phoneNumber, messageBody, function(error, message){
+			if(error)
+			{
+				throw Error(error);
+			}
+			if(!message)
+			{
+				return(res.render('index', { submitted : false, 
+											 error: "Oops! The phone number, " + req.body.phoneNumber + ", does not exists!"
+													}));
+			}
+
+		});
+		WarmUp.addRecipient(req.body.phoneNumber, req.body.zipcode, req.body.notificationTemprature, function(error, status){
+			if(error)
+			{
+				throw Error(error);
+			}
+			if(status == "success")
+			{
+				res.render('index', { submitted : true});
+			}
+		});		
 	});
 }
