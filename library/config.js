a601e365af359f5d2dad8e1bb4bccb11a7fb18ae
Yohannes <personalprojectyohannes@gmail.com>
2015-01-20 21:51:37 -0600

    [TASK] remove config file

--- library/config.js
+++
@@ -1,7 +0,0 @@
-var accountSid = 'AC7e4b2165797346a1c01ff3dba72e5a27'; 
-var authToken = 'aceffca115a7dcb55063b7b3e8db8f0c'; 
-var client = require('twilio')(accountSid, authToken);
-
-exports.accountSid = accountSid;
-exports.authToken = authToken;
-exports.client = client;
