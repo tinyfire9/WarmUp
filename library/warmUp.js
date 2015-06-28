20f986f03930f6600ed0284e208d9943662b0105
slimfire <personalprojectyohannes@gmail.com>
2015-06-27 23:20:50 -0700

    [FEATURE] add method to handle unsubscribtion and subscribtion information update

--- library/warmUp.js
+++ library/warmUp.js
@@ -1,21 +1,15 @@
-var models = require('../models/model.js');
-var clientModel = models.clientModel;
+var clientModel = require('../models/model.js');
 
-var PublicApis = require('./publicApis.js');
-var Recipient = require('./recipient.js');
+var Temprature = require('./temprature.js'),
+	Subscribe = require('./subscribe.js'),
+	Sms = require('./sms.js');
 
 var WarmUp = function(){}
 
-WarmUp.prototype = Object.create(Recipient.prototype);
-
-WarmUp.prototype.sendsms = PublicApis.prototype.sendsms;
-
-WarmUp.prototype.currentTemprature = PublicApis.prototype.currentTemprature;
-
-WarmUp.prototype.checkTempratureAndSendSMS = function(){
-	var messageBody;
-	var degreeSign = String.fromCharCode(parseInt("00B0", 16));
-	WarmUp.prototype.queryRecipient(function(error, recipients){
+WarmUp.prototype.checkTempratureAndSmsTemprature = function(){
+	var messageBody,
+			degreeSign = String.fromCharCode(parseInt("00B0", 16));
+	Subscribe.querySubscribers(function(error, recipients){
 		if(error)
 		{
 			throw Error(error);
@@ -24,28 +18,140 @@ WarmUp.prototype.checkTempratureAndSendSMS = function(){
 		{	
 			for(var i = 0; i < recipients.length; i++)
 			{
-				(function(i)
-				{
-					WarmUp.prototype.currentTemprature(recipients[i].zipcode, function(error, weatherData){
-						if(error)
-						{
-							throw Error(error);
-						}
-						if(weatherData.temprature < recipients[i].notificationTemprature)
-						{
-							messageBody = "Hey, the weather in " + weatherData.city + " is " + weatherData.temprature + " " + degreeSign + "F. -Sent from warmUp."
-							WarmUp.prototype.sendsms(recipients[i].phoneNumber, messageBody, function(error, message){
-								if(error)
-								{
-									throw Error(error);
-								}
-							});
-						}
-					});
-            	})(i)
+
+				Temprature.checkTemprature(recipients[i].zipcode, function(error, weatherData){
+					if(error)
+					{
+						throw Error(error);
+					}
+					if(weatherData.temprature < recipients[i].notificationTemprature)
+					{
+						messageBody = "Hey, the weather in " + weatherData.city + " is " + weatherData.temprature + " " + degreeSign + "F. -Sent from warmUp."
+						Sms.sendSms(recipients[i].phoneNumber, messageBody, function(error, message){
+							if(error)
+							{
+								throw Error(error);
+							}
+							console.log('Message sent to subscriber with this information, [' + recipients[i].phoneNumber + ', ' + weatherData.city + weatherData.temprature + '] at ' + new Date().toString());
+						});
+					}
+				});
 			}	
 		}
 	});
 }
 
-module.exports = WarmUp;
+WarmUp.prototype.checkTempratureAndSmsWelcomeMessage = function(phoneNumber, zipcode, notificationTemprature, callback){
+	var messageBody,
+		degreeSign = String.fromCharCode(parseInt("00B0", 16));
+	Temprature.checkTemprature(zipcode, function(error, weatherData){
+		if(error)
+		{
+			throw Error(error);
+		}
+		if(!weatherData)
+		{
+			callback({ submitted : false, error: "Oops! The zipcode, " + zipcode + ", does not exists!" });
+		}
+		messageBody = "Hi, thank you for subscribing! The current temprature in " + weatherData.city + " is " +
+					   weatherData.temprature + " " + degreeSign + "F. We will notify you every 12 hours, if the temprature is below " + 
+					   notificationTemprature + " " + degreeSign + "F. -Sent from warmUp";
+		Sms.sendSms(phoneNumber, messageBody, function(error, message){
+			if(error)
+			{
+				throw Error(error);
+			}
+			if(message)
+			{
+				Subscribe.addSubscriber(phoneNumber, zipcode, notificationTemprature, function(error, subscribtion){
+					if(error)
+					{
+						throw Error(error);
+					}
+					if(subscribtion)
+					{
+						callback(true);
+					}
+					else
+					{
+						callback(false);
+					}
+				});
+			}
+			else
+			{
+				callback({ submitted : false, error: "Oops! The phone number, " + phoneNumber + ", does not exists!" });
+			}
+		});
+	});
+}
+
+WarmUp.prototype.checkTempratureAndSmsUpdate = function(phoneNumber, zipcode, notificationTemprature, callback){
+	var messageBody,
+		update,
+		degreeSign = String.fromCharCode(parseInt("00B0", 16));
+	Temprature.checkTemprature(zipcode, function(error, weatherData){
+		if(error)
+		{
+			throw Error(error);
+		}
+		if(!weatherData)
+		{
+			callback({ submitted : false, error: "Oops! The zipcode, " + zipcode + ", does not exists!" });
+		}
+		messageBody = "Thank you for updating your notification preferences! Your current subscribtion information is as follows :\n\tPhone number : " +
+					phoneNumber + ", \n\t zipcode : " + zipcode + "\n\t Notification temprature : " + notificationTemprature + " " + degreeSign + "F. -Sent from warmUp";
+		Sms.sendSms(phoneNumber, messageBody, function(error, message){
+			if(error)
+			{
+				throw Error(error);
+			}
+			if(!message)
+			{
+				callback({ submitted : false, error: "Oops! The phone number, " + phoneNumber + ", does not exists!"});
+			}
+			else
+			{
+				update = {
+					zipcode : zipcode,
+					notificationTemprature : notificationTemprature
+				};
+				Subscribe.updateSubscriberInfo(phoneNumber, update, function(success){
+					if(success)
+					{
+						callback(true);
+					}
+					else
+					{
+						callback(false);
+					}
+				});
+			}
+
+		});
+	});
+}
+
+WarmUp.prototype.smsUnsubscribtion = function(phoneNumber, callback){
+	var messageBody = 'We miss you :( ! - Sent from warmUp';
+	Sms.sendSms(phoneNumber, messageBody, function(error, message){
+		if(error)
+		{
+			throw Error(error);
+		}
+		if(message)
+		{
+			Subscribe.removeSubscriber(phoneNumber, function(subscribtion){
+				if(subscribtion == true)
+				{
+					callback(true);
+				}
+				else
+				{
+					callback(false);
+				}
+			});
+		}
+	});
+}
+module.exports = new WarmUp();
