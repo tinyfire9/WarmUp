2135c184a32db320bca8750a00fba18b7c83779b
slimfire <personalprojectyohannes@gmail.com>
2015-06-29 21:05:18 -0700

    [FEATURE] add response handlers on user unsubscription and user subscription info update

--- controller/routes.js
+++ controller/routes.js
@@ -1,50 +1,86 @@
-var WarmUp = require('../library/warmUp.js');
-
-var WarmUp = new WarmUp();
+var WarmUp = require('../library/warmUp.js'),
+	Subscribe = require('../library/subscribe.js'),
+	statusCodes = require('../library/statusCodes.js');
 
 exports.indexResponseHandler = function(req, res){
-	res.render('index');
+	res.render('layout', {statusCode : statusCodes.index.appIndex});
 }
 
-exports.addRecipientResponseHandler = function(req, res){
-	var messageBody;
-	var degreeSign = String.fromCharCode(parseInt("00B0", 16));
-	WarmUp.currentTemprature(req.body.zipcode, function(error, weatherData){
-		if(error)
-		{
-			throw Error(error);
-		}
-		if(!weatherData)
-		{
-			return (res.render('index', { submitted : false,
-								  error: "Oops! The zipcode, " + req.body.zipcode + ", does not exists!"
-								}));
-		}
-		messageBody = "Hi, thank you for subscribing! The current temprature in " + weatherData.city + " is " +
-					   weatherData.temprature + " " + degreeSign + "F. We will notify you every 12 hours, if the temprature is below " + 
-					   req.body.notificationTemprature + " " + degreeSign + "F. -Sent from warmUp";
-		WarmUp.sendsms(req.body.phoneNumber, messageBody, function(error, message){
-			if(error)
-			{
-				throw Error(error);
-			}
-			if(!message)
-			{
-				return(res.render('index', { submitted : false, 
-											 error: "Oops! The phone number, " + req.body.phoneNumber + ", does not exists!"
-													}));
-			}
+exports.subscriptionResponseHandler = function(req, res){
+	res.render('layout', {statusCode : statusCodes.index.subscriptionIndex});
+}
 
-		});
-		WarmUp.addRecipient(req.body.phoneNumber, req.body.zipcode, req.body.notificationTemprature, function(error, status){
-			if(error)
-			{
-				throw Error(error);
-			}
-			if(status == "success")
-			{
-				res.render('index', { submitted : true});
-			}
-		});		
+exports.addSubscriptionResponseHandler = function(req, res){
+	var phoneNumber = req.body.phoneNumber,
+		zipcode = req.body.zipcode,
+		notificationTemprature = req.body.notificationTemprature;
+	WarmUp.checkTempratureAndSmsWelcomeMessage(phoneNumber, zipcode, notificationTemprature, function(response){
+		console.log(response);
+		res.render('layout', response);
+	});
+}
+
+exports.updateSubscriptionResponseHandler = function(req, res){
+	res.render('layout', {statusCode : statusCodes.index.updateSubscriptionIndex});
+}
+
+exports.updateSubscriptionInfoResponseHandler = function(req, res){
+	Subscribe.querySubscriber(req.body.phoneNumber, function(subscriberInfo){
+		if(subscriberInfo)
+		{
+			res.render('layout', {
+				statusCode : statusCodes.subscription.updateSubscription, 
+				subscriberInfo : subscriberInfo
+			});
+		}
+		else
+		{
+			res.render('layout', {
+				statusCode : statusCodes.error.error,
+				error : 'Sorry! We cound\'t find you phone number. Try again!',
+				subscriberInfo : null
+			});
+		}
+	});
+}
+
+exports.updateSubscriptionStatusResponseHandler = function(req, res){
+	var phoneNumber = req.params.phoneNumber, 
+		zipcode = req.body.zipcode,
+		notificationTemprature = req.body.notificationTemprature;
+	WarmUp.checkTempratureAndSmsUpdate(phoneNumber, zipcode, notificationTemprature, function(response){
+		res.render('layout', response);
+	});
+}
+
+exports.unsubscriptionResponseHandler = function(req, res){
+	res.render('layout', {statusCode : statusCodes.index.unsubscriptionIndex, error : null});
+}
+
+exports.unsubscriptionInfoResponseHandler = function(req, res){
+	var phoneNumber = req.body.phoneNumber;
+	Subscribe.querySubscriber(phoneNumber, function(subscriberInfo){
+		if(subscriberInfo)
+		{
+			console.log(subscriberInfo);
+			res.render('layout', {
+				statusCode : statusCodes.subscription.unsubscription,
+				subscriberInfo : subscriberInfo, 
+				error : null
+			});
+		}
+		else
+		{
+			res.render('layout', {
+				statusCode : statusCodes.error.error, 
+				error : 'Phone number not found. Please check and try again. Thank you!'})
+		}
+	});
+}
+
+exports.unsubscriptionStatusResponseHandler = function(req, res){
+	var phoneNumber = req.params.phoneNumber;
+	WarmUp.smsUnsubscription(phoneNumber, function(response){
+		res.render('layout', response);
 	});
 }
