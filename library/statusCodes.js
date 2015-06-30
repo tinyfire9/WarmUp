ade1b3bc1ff522547c010f1ebe20874a262f89bb
slimfire <personalprojectyohannes@gmail.com>
2015-06-29 21:29:06 -0700

    [FEATURE] store status codes in object to render different views

---
+++ library/statusCodes.js
@@ -0,0 +1,26 @@
+var index = {
+	appIndex : 'appIndex',
+	subscriptionIndex : 'subscriptionIndex',
+	updateSubscriptionIndex : 'updateSubscriptionIndex',
+ 	unsubscriptionIndex : 'unsubscriptionIndex'
+},
+	subscription = {
+	addSubscription : 'addSubscription', 
+	updateSubscription : 'updateSubscription', 
+	unsubscription : 'unsubscription'
+},
+	success = {
+	subscriptionSuccess : 'subscriptionSuccess', 
+	updateSubscriptionSuccess : 'updateSubscriptionSuccess', 
+	unsubscriptionSuccess : 'unsubscriptionSuccess'
+},
+	error = {
+	error : 'error'
+};
+
+module.exports = {
+	index : index,
+	subscription : subscription,
+	success : success,
+	error : error
+};
